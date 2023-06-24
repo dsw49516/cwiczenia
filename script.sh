eae45c9811f89951613f72f4ece545b1cb4cb01a
dsw49516SM <dsw49516@student.dsw.edu.pl>
2023-06-24 12:56:56 +0200

    Zmodyfikowano script.sh
    Prints 100 logs

--- script.sh
+++ script.sh
@@ -5,9 +5,25 @@ show_date() {
     echo "Dzisiejsza data: $current_date"
 }
 
+create_logs() {
+    local count=${1:-100}
+    mkdir "logs"
+    cd logs
+    for ((i = 1; i <= count; i++)); do
+        filename="log${i}_$(basename "$0")_$(date +%Y-%m-%d).txt"
+        echo "Nazwa pliku: $filename" > "$filename"
+        echo "Nazwa skryptu: $0" >> "$filename"
+        echo "Data: $(date)" >> "$filename"
+    done
+    cd ..
+}
+
 case "$1" in
     --date)
         show_date
+        ;;
+    --logs)
+        create_logs
         ;;
     *)
         echo "Nieznana opcja."
