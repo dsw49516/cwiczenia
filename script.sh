a2c2012e99d1f2d33f762c76dabae0222d82379d
dsw49516SM <dsw49516@student.dsw.edu.pl>
2023-06-24 13:17:52 +0200

    Modified script.sh
    Added --init and PATH setting

--- script.sh
+++ script.sh
@@ -9,7 +9,8 @@ show_help() {
     echo "Dostępne opcje:"
     echo "  --date, -d               Wyświetla dzisiejszą datę"
     echo "  --logs, -l               Tworzy 100 plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę"
-    echo "  --logs <n>, -l <n>          Tworzy <n> liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę"
+    echo "  --logs <n>, -l <n>       Tworzy <n> liczbę plików logx.txt, zawierających nazwę pliku, nazwę skryptu i datę"
+    echo "  --init                   Klonuje całe repozytorium i ustawia ścieżkę w zmiennej PATH."
     echo "  --help, -h               Wyświetla tę wiadomość"
 }
 
@@ -26,6 +27,19 @@ create_logs() {
     cd ..
 }
 
+clone_repo_and_set_path() {
+    local repo_url="<adres_repozytorium>"
+    local repo_dir="repo"
+    local repo_name="<nazwa_repozytorium>"
+
+    git clone "$repo_url" "$repo_dir"
+
+    echo "export PATH=\"\$PATH:$(pwd)/$repo_dir\"" >> ~/.bashrc
+    source ~/.bashrc
+
+    echo "Repozytorium \"$repo_name\" zostało sklonowane i ścieżka została dodana do zmiennej PATH."
+}
+
 case "$1" in
     --date)
         show_date
@@ -38,6 +52,9 @@ case "$1" in
         ;;
     -l)
         create_logs "$2"
+        ;;
+    --init)
+        clone_repo_and_set_path
         ;;
     --help)
         show_help
